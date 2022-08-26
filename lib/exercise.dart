0bfcdd5d197e72e7be57bf21e33008f97f892015
abhishek-baj <abhishekbajpai680@gmail.com>
2022-08-26 11:16:33 +0530

    intial commit

---
+++ lib/exercise.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+
+class  exercise{
+  final String final Time,difficlt, image;
+
+  excerise({
+    @required time;
+    @required difficult;
+    @required image;
+})
+
+}
+
+
+
+
